ce11688cf1d0b42f69f6489f802f90ffc1584b4e
William Pispico <williampispico@gmail.com>
2023-02-18 12:32:39 -0400

    changing terraform workspace and variables file

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "redrocket"
+    workspaces {
+      name = "terraform-lab"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
